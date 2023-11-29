2b32160f1dc274528666d41e463c1ba8cafb293b
NICKY RASAMOELINA <nickyrasa@gmail.com>
2023-11-29 10:23:20 +0100

    quete9

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -9,23 +9,30 @@ use Doctrine\Persistence\ObjectManager;
 
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
+
+    public const SERIE = [
+        ['title' => 'One Piece', 'synopsis' => 'Des débiles sillonnent la mer', 'category' => 'category_Aventure'],
+        ['title' => 'One Piece', 'synopsis' => 'Des débiles sillonnent la mer', 'category' => 'category_Aventure'],
+        ['title' => 'One Piece', 'synopsis' => 'Des débiles sillonnent la mer', 'category' => 'category_Aventure'],
+        ['title' => 'One Piece', 'synopsis' => 'Des débiles sillonnent la mer', 'category' => 'category_Aventure'],
+        ['title' => 'One Piece', 'synopsis' => 'Des débiles sillonnent la mer', 'category' => 'category_Aventure']
+    ];
     public function load(ObjectManager $manager)
     {
-        $program = new Program();
-        $program->setTitle('Walking dead');
-        $program->setSynopsis('Des zombies envahissent la terre');
-        $program->setCategory($this->getReference('category_Action'));
-        $manager->persist($program);
+        foreach (self::SERIE as $programName) {
+            $program = new Program();
+            $program->setTitle($programName['title']);
+            $program->setSynopsis($programName['synopsis']);
+            $program->setCategory($this->getReference($programName['category']));
+            $manager->persist($program);
+        }
         $manager->flush();
     }
 
     public function getDependencies()
     {
         return [
-          CategoryFixtures::class,
+            CategoryFixtures::class,
         ];
     }
-
-
 }
-
